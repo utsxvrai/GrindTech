ae50d0e05fe9d02686cfaba448edce877d14e3b6
Utsav Rai <utsavrai2115@gmail.com>
2025-12-13 11:37:13 +0530

    refactor : changed deployed api refference

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-    //   "https://grindtech.vercel.app",
+      "https://grindtech.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL || 'https://grindtech.onrender.com/api/v1',
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL || 'https://grindtech.onrender.com/api/v1',
+    // baseURL: 'http://localhost:3000/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
